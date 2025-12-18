2db98e06afcd739c7c1dfa83e64f25513e3250c2
RSS9190 <rscanlon919@gmail.com>
2025-12-17 19:55:58 -0500

    Initial Commit

---
+++ BTCapp/BTCappApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCappApp.swift
+//  BTCapp
+//
+//  Created by Ron Scanlon on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BTCappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTCappTests/BTCappTests.swift
@@ -0,0 +1,17 @@
+//
+//  BTCappTests.swift
+//  BTCappTests
+//
+//  Created by Ron Scanlon on 12/17/25.
+//
+
+import Testing
+@testable import BTCapp
+
+struct BTCappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
